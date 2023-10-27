226eb5fc00573e41e94f2fa8123402aae7160348
Harsha Vanga <H.Vanga@tees.ac.uk>
2023-10-27 14:56:52 +0100

    Topic Addition implemented

--- forum/views.py
+++ forum/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect, redirect
 from .models import Topic, Comment
+from home.models import Tickers
 from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='signin')
@@ -7,5 +8,20 @@ def add(request, id):
     if request.method == 'GET':
         return render(request, 'forum/add.html', {'id': id})
     elif request.method == 'POST':
+        topic = request.POST['topic'].strip()
+        description = request.POST['description'].strip()
+        if topic == '':
+            return render(request, 'forum/add.html',{
+                'message': "Title Cannot be Empty",
+                'id': id})
+        
+        parent = Tickers.objects.get(id = id)
+        item = Topic(
+            title = topic,
+            description = description,
+            parent_ticker = parent,
+            author = request.user
+        )
+        Tickers.save(item)
         return redirect(f'/share/{id}')
 
